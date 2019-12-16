9b7dd7382b8820b4d6ea2d0dc5ab70b100793201
gtkatakura <gt.katakura@gmail.com>
2019-12-15 22:43:57 -0300

    feat: rules for eslintrc

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,15 +2,28 @@ const path = require('path')
 
 module.exports = {
   "extends": [
-    "react-app",
+    "airbnb",
     "plugin:import/errors",
-    "plugin:import/warnings"
+    "plugin:import/warnings",
   ],
+  "rules": {
+    "arrow-parens": "off",
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        "max": 1,
+        "maxEOF": 1,
+      },
+    ],
+    "semi": ["error", "never"],
+    "import/prefer-default-export": "off",
+    "react/jsx-filename-extension": "off"
+  },
   "settings": {
     "import/resolver": {
       "node": {
-        "paths": [path.join(__dirname, 'src')]
-      }
-    }
-  }
+        "paths": [path.join(__dirname, 'src')],
+      },
+    },
+  },
 }
